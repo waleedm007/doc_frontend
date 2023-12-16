5c55fd60d3ba92718fa03ab4f572f5a33044bd97
waleedm007 <138612573+waleedm007@users.noreply.github.com>
2023-12-16 19:41:29 +0500

    update

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'https://admin-doc.thesuperstarshop.com/api', // Replace this with your actual base URL
+    apiUrl: 'http://admin-doc.thesuperstarshop.com/api', // Replace this with your actual base URL
   };
   
