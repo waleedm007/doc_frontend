9655124cf93012f9311ff066332cc101b191a17c
waleedm007 <138612573+waleedm007@users.noreply.github.com>
2023-12-16 19:08:53 +0500

    upload

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000/api', // Replace this with your actual base URL
+  apiUrl: 'https://127.0.0.1:8000/api', // Replace this with your actual base URL
 };
