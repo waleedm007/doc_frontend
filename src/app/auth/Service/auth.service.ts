2cdf78bae440e43df3e60e63e692a512cf599698
Ali Ahmed <37513011+Alideveloper49@users.noreply.github.com>
2023-12-18 15:54:51 +0500

    clean code

--- src/app/auth/Service/auth.service.ts
+++ src/app/auth/Service/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { environment } from 'src/app/environments/environment';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
--- src/environments/environment.online.ts
+++ src/environments/environment.online.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production: true,
-    apiUrl: 'https://admin-doc.thesuperstarshop.com/api/',
+    apiUrl: 'https://admin-doc.thesuperstarshop.com/api',
   };
   
